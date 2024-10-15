fcdcc7ed232e242faa6635553ac1f5cef7c6fbfe
mh2011-maisie <mh2011.music@gmail.com>
2024-10-15 15:44:54 +0100

    Part 2 and Part 3

--- index.test.js
+++ index.test.js
@@ -21,6 +21,31 @@ describe('./musicians endpoint', () => {
         const responseData = await JSON.parse(response.text);
         expect(responseData[0].name).toBe("Mick Jagger")
     });
+    test("Creates correct data at /musicians/:id", async function (){
+        const musician = await Musician.create({ name: "Paul McCartney", instrument: "Bass" });
+
+        const response = await request(app).get(`/musicians/${musician.id}`);
+        expect(response.statusCode).toBe(200)
+        expect(response.body.name).toBe("Paul McCartney")
+    });
+    test("Updates correct data at /musicians", async function (){
+        await Musician.update({ instrument: "Bass Guitar" }, {where : {name: "Paul McCartney"}});
+        
+        const updatedMusician = await Musician.findOne({where: {name: "Paul McCartney"}})
+        const response = await request(app).get(`/musicians/${updatedMusician.id}`);
+        expect(response.statusCode).toBe(200)
+        expect(response.body.instrument).toBe("Bass Guitar")
+    });
+    
+    test("Deletes correct data at /musicians/:id", async function (){
+        
+        const preDeletedMusician = await request(app).get("/musicians");
+
+        await Musician.destroy({where : {name: "Paul McCartney"}});
+        const deletedMusician = await request(app).get("/musicians");
+        expect(deletedMusician.statusCode).toBe(200)
+        expect(deletedMusician.body.length).toBe(preDeletedMusician.body.length - 1)
+    });
     
     
 
--- src/app.js
+++ src/app.js
@@ -10,10 +10,35 @@ app.get("/musicians", async (request, response)=>{
     const musicians = await Musician.findAll();
     response.json(musicians)
 })
+app.get("/musicians/:id", async (req,res) =>{
+    let musician = await Musician.findByPk(Number(req.params.id));
+    res.json(musician) 
+})
 
 
+app.use(express.json());
+app.use(express.urlencoded());
 
+app.post('/musicians', async (req,res) =>{
+    await Musician.create(req.body);
+    const musicians = await Musician.findAll();
 
+    res.json(musicians)
+})
+app.put('/musicians/:id', async (req,res) =>{
+
+    await Musician.update(req.body, {where: {id :req.params.id}});
+    const musicians = await Musician.findAll();
+
+    res.json(musicians)
+})
+
+app.delete('/musicians/:id', async (req,res) =>{
+    await Musician.destroy({where: {id : req.params.id}});
+    const musicians = await Musician.findAll();
+
+    res.json(musicians)
+})
 
 
 module.exports = app;
